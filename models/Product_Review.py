2c918dab8581e0b8565286eebc0298472810587a
joseph-galante <jgalante00@gmail.com>
2021-05-24 18:26:10 -0400

    product reviews to json

--- models/Product_Review.py
+++ models/Product_Review.py
@@ -1,4 +1,5 @@
 from .db import db
+import models
 
 class Product_Review (db.Model):
     __tablename__ = 'product_reviews'
@@ -6,3 +7,9 @@ class Product_Review (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     review_id = db.Column(db.Integer, db.ForeignKey('reviews.id'))
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+    def to_json (self):
+        review = models.Review.query.filter(models.Review.id == self.review_id).first()
+        return {
+            "review": review.to_json()
+        }
