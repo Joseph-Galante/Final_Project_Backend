a6b7a1e6ea554c0729acb06781c9aa9f42a77015
Joe <jgalante00@gmail.com>
2021-05-26 16:10:09 -0400

    app port fix

--- application.py
+++ application.py
@@ -20,5 +20,5 @@ app.route('/', methods=["GET"])(root)
 apply_routes(app)
 
 if __name__ == '__main__':
-  port = os.environ.get('PORT') or 5000
-  app.run(port=port, debug=True)
+  port = int(os.environ.get('PORT') or 5000)
+  app.run(host='0.0.0.0', port=port, debug=True)
